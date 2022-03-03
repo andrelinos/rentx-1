ce5e574d110cc8fca464ff7f0a1c29090fbb2d7b
nosima <eduardo_nosima@yahoo.com.br>
2022-03-03 11:35:25 -0300

    React Native - Chapter IV - Sincronização - Fix

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import { TextInputProps } from 'react-native';
+import { useTheme } from 'styled-components';
+import { Feather } from '@expo/vector-icons';
+
+import {
+  Container,
+  InputText,
+  IconContainer
+} from './styles';
+
+interface Props extends TextInputProps {
+  iconName: React.ComponentProps<typeof Feather>['name'];
+  value?: string;
+}
+
+export function Input({
+  iconName,
+  value,
+  ...rest
+} : Props ){
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+
+  const theme = useTheme();
+
+  function handleInputFocused() {
+    setIsFocused(true);
+  }
+
+  function handleInputBlur() {
+    setIsFocused(false);
+
+    setIsFilled(!!value);
+    // O codigo abaixo eh igual ao de cima
+    /*if(value) {
+      setIsFilled(true)
+    } else {
+      setIsFilled(false)
+    }*/
+  }
+
+  return (
+    <Container>
+      <IconContainer isFocused={isFocused}>
+        <Feather 
+          name={iconName}
+          size={24}
+          color={isFocused || isFilled ? theme.colors.main : theme.colors.text_detail}
+        />
+      </IconContainer>
+
+      <InputText
+        onFocus={handleInputFocused}
+        onBlur={handleInputBlur}
+        isFocused={isFocused}
+        {...rest}
+      />
+    </Container>
+  );
+}
---
+++ src/screens/SignUp/SignUpSecondStep/index.tsx
@@ -0,0 +1,122 @@
+import React, { useState } from 'react';
+import { 
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
+  Alert
+} from 'react-native';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { useTheme } from 'styled-components';
+import { api } from '../../../services/api';
+
+import { BackButton } from '../../../components/BackButton';
+import { Bullet } from '../../../components/Bullet';
+import { PasswordInput } from '../../../components/PasswordInput';
+import { Button } from '../../../components/Button';
+
+interface Params {
+  user: {
+    name: string;
+    email: string;
+    driverLicense: string;
+  }
+}
+
+import {
+  Container,
+  Header,
+  Steps,
+  Title,
+  SubTitle,
+  Form,
+  FormTitle
+} from './styles';
+
+export function SignUpSecondStep(){
+  const [password, setPassword] = useState('');
+  const [passwordConfirm, setPasswordConfirm] = useState('');
+
+  const theme = useTheme();
+  const navigation = useNavigation();
+  const route = useRoute();
+
+  const { user } = route.params as Params;
+
+  function handleBack() {
+    navigation.goBack();
+  }
+
+  async function handleRegister() {
+    if(!password || !passwordConfirm) {
+      return Alert.alert('Informe a senha e a confirmação.');
+    }
+
+    if(password != passwordConfirm) {
+      return Alert.alert('As senhas não são iguais.');
+    }
+
+    await api.post('/users', {
+      name: user.name,
+      email: user.email,
+      password,
+      driver_license: user.driverLicense
+    })
+    .then(() => {
+      navigation.navigate('Confirmation', {
+        title: 'Conta Criada!',
+        message: `Agora é só fazer login\ne aproveitar.`,
+        nextScreenRoute: 'SignIn'
+      });
+    })
+    .catch(() => {
+      Alert.alert('Opa','Não foi possível cadastrar');
+    });
+  }
+
+  return (
+    <KeyboardAvoidingView behavior="position" enabled>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss} >
+        <Container>
+          <Header>
+            <BackButton onPress={handleBack}/>
+            <Steps>
+              <Bullet active />
+              <Bullet />
+            </Steps>
+          </Header>
+
+          <Title>
+            Crie sua{'\n'}
+            conta
+          </Title>
+          <SubTitle>
+            Faça seu cadastro de{'\n'}
+            forma rápida e fácil
+          </SubTitle>
+
+          <Form>
+            <FormTitle>2. Senha</FormTitle>
+            <PasswordInput 
+              iconName="lock"
+              placeholder="Senha"
+              onChangeText={setPassword}
+              value={password}
+            />
+            <PasswordInput 
+              iconName="lock"
+              placeholder="Repetir Senha"
+              onChangeText={setPasswordConfirm}
+              value={passwordConfirm}
+            />
+          </Form>
+
+          <Button 
+            title="Cadastrar"
+            color={theme.colors.success}
+            onPress={handleRegister}
+          />
+        </Container>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>      
+  );
+}
